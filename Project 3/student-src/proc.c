d12b10cc65e39ab4448801c3df28eeacd3719bf1
Andrew Friedman <andrewfriedman01@gmail.com>
2022-03-15 00:17:08 -0400

    Solve problem 3 - proj 3

--- Project 3/student-src/proc.c
+++ Project 3/student-src/proc.c
@@ -11,14 +11,14 @@
 /**
  * --------------------------------- PROBLEM 3 --------------------------------------
  * Checkout PDF section 4 for this problem
- * 
+ *
  * This function gets called every time a new process is created.
  * You will need to allocate a frame for the process's page table using the
  * free_frame function. Then, you will need update both the frame table and
- * the process's PCB. 
- * 
- * @param proc pointer to process that is being initialized 
- * 
+ * the process's PCB.
+ *
+ * @param proc pointer to process that is being initialized
+ *
  * HINTS:
  *      - pcb_t: struct defined in pagesim.h that is a process's PCB.
  *      - You are not guaranteed that the memory returned by the free frame allocator
@@ -26,21 +26,26 @@
  * ----------------------------------------------------------------------------------
  */
 void proc_init(pcb_t *proc) {
-    // TODO: initialize proc's page table.
+    (*proc).saved_ptbr = free_frame(); // update the process's PCB
+    // guaranteed that the memory returned by the free frame allocator is empty:
+    for (int i = (*proc).saved_ptbr; i < (*proc).saved_ptbr + PAGE_SIZE; i++) {
+      mem[i] = 0;
+    }
+    (*frame_table).process = proc; // update the frame table
 }
 
 /**
  * --------------------------------- PROBLEM 4 --------------------------------------
  * Checkout PDF section 5 for this problem
- * 
- * Switches the currently running process to the process referenced by the proc 
+ *
+ * Switches the currently running process to the process referenced by the proc
  * argument.
- * 
+ *
  * Every process has its own page table, as you allocated in proc_init. You will
  * need to tell the processor to use the new process's page table.
- * 
+ *
  * @param proc pointer to process to become the currently running process.
- * 
+ *
  * HINTS:
  *      - Look at the global variables defined in pagesim.h. You may be interested in
  *      the definition of pcb_t as well.
@@ -53,10 +58,10 @@ void context_switch(pcb_t *proc) {
 /**
  * --------------------------------- PROBLEM 8 --------------------------------------
  * Checkout PDF section 8 for this problem
- * 
+ *
  * When a process exits, you need to free any pages previously occupied by the
  * process.
- * 
+ *
  * HINTS:
  *      - If the process has swapped any pages to disk, you must call
  *      swap_free() using the page table entry pointer as a parameter.
