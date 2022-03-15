d297f409b4acb4844ce58174ee549fea9a1d2b88
Andrew Friedman <andrewfriedman01@gmail.com>
2022-03-14 22:06:29 -0400

    Solve Problem 1 - Proj 3

--- Project 3/student-src/va_splitting.h
+++ Project 3/student-src/va_splitting.h
@@ -10,20 +10,25 @@
  * Checkout PDF Section 3 For this Problem
  *
  * Split the virtual address into its virtual page number and offset.
- * 
- * HINT: 
- *      -Examine the global defines in pagesim.h, which will be necessary in 
+ *
+ * HINT:
+ *      -Examine the global defines in pagesim.h, which will be necessary in
  *      implementing these functions.
  * ----------------------------------------------------------------------------------
  */
+ // The system has 16KB pages and a 24-bit virtual address space.
+ // addr is int32
+ // (8)????????(10)NNNNNNNNNN(14)OOOOOOOOOOOOOO
 static inline vpn_t vaddr_vpn(vaddr_t addr) {
-    // TODO: return the VPN from virtual address addr.
-    return 0;
+    // get rid of left 8 bits
+    // move 10 bits to far right (get rid of right 14 bits)
+    return addr << 8 >> 22; // 2^14 = 16kb
 }
 
 static inline uint16_t vaddr_offset(vaddr_t addr) {
-    // TODO: return the offset into the frame from virtual address addr.
-    return 0;
+    // get rid of left 18 bits
+    // move 14 bits to far right
+    return addr << 18 >> 18; // 32 - 14 = 18
 }
 
-#pragma GCC diagnostic pop
+#pragma GCC diagnostic
